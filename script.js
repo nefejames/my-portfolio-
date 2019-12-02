da15c4d88415a652d403b7c5d5bde00146a57993
Nefe Emadamerho <nefejames@yahoo.com>
2019-12-01 18:28:52 -0800

    added theme switcher

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const burger = document.getElementById("burger");
-const closeNav = document.getElementById("close-nav");
-const nav = document.querySelector("nav");
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
 
-burger.addEventListener("click", () => {
-  nav.style.transform = "translateY(0)";
-});
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
 
-closeNav.addEventListener("click", () => {
-  nav.style.transform = "translateY(-100%)";
-});
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false
+
+);
